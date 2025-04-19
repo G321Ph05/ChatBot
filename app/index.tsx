15531f4a8e6ead89bbfafe925d2f072c42a53171
Gia-Pham <billpham657@gmail.com>
2025-04-19 12:03:30 -0700

    WelcomeScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,47 @@
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Text, View, ImageBackground, TouchableOpacity, SafeAreaView, Image, useWindowDimensions } from "react-native";
+import React from 'react'
+import { StatusBar } from "expo-status-bar"
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
 
-export default function Index() {
-  const API_key = "";
-  
+const WelcomeScreen = () => {
+  const windowHeight  = useWindowDimensions().height;
+
   return (
-    <View style={styles.text}>
-      <Text>Hello</Text>
-      <Text>Good Morning</Text>
-    </View>
+    <ImageBackground
+      source={require("./Blue_Abstact_BG.png")}
+      className="flex-1"
+    >
+      <Image source={require("./chatbot_icon_image.png")} 
+      style={{
+          alignSelf: "center",
+          justifyContent: "center",
+          width: 185,
+          height: 185,
+          marginTop: windowHeight * 0.29,
+      }}>
+      </Image>
+      <LinearGradient
+      colors={["transparent", "rgba(0, 0, 0, 0.5)", "black"]}
+      style={{flex: 1, height: "100%", width: "100%" }}
+      >
+        <View className="w-full mt-10"/>
+        <SafeAreaView>
+          <View className="px-5 absolute mt-7">
+              <Text className="text-5xl text-white font-extrabold">Explore your Question with AI-powered Voice</Text>
+              <Text className="text-lg font-semibold text-white/50">Speakup offers a variety of customizable, natural-sounding voices accomodated your specific needs </Text>
+            <TouchableOpacity className="bg-primary-600 justify-center self-center 
+                                        py-5 px-3 rounded-full w-full mt-14"
+                              onPress={() => router.push("/(home)")}
+                                        >
+                  <Text className="text-white text-center text-lg font-semibold">Get Started</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </LinearGradient>
+      <StatusBar style="light"/>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  input: {
-    height: 20, 
-    color: "black",
-    borderStyle: "solid"
-  }
-})
+export default WelcomeScreen
