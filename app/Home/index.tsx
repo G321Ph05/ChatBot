955abf5b0bf3f9387b53cc8e08fab5b6e61b3d5c
Hai Phan <27phanh@gmail.com>
2025-04-19 08:58:15 -0700

    add voice file and index files

---
+++ app/Home/index.tsx
@@ -0,0 +1,14 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {    
+    return (
+        <linearGradient
+            colors={["#5e35b1", "#67ab7", "#ab47c"]}
+            style={{flex: 2, height: "100%", width: "1000%"}} 
+        >
+            <Text>Home</Text>
+        </linearGradient>
+    );
+};
